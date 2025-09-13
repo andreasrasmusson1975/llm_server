8232f31bf2ea527d5d6eec44ad6783030480810a
andreasrasmusson1975 <38373758+andreasrasmusson1975@users.noreply.github.com>
2025-09-13 20:14:42 +0200

    Minor changes

--- setup.py
+++ setup.py
@@ -6,22 +6,6 @@ setup(
     description="A server for managing and deploying large language models.",
     author="Andreas Rasmusson",
     packages=find_packages(),
-    install_requires=[
-        "fastapi>=0.111.0",
-        "uvicorn>=0.30.0",
-        "transformers>=4.56.0",
-        "azure-identity>=1.15.0",
-        "azure-keyvault-secrets>=4.8.0",
-        "pyyaml>=6.0",
-        "bitsandbytes>=0.43.2",
-        "accelerate>=1.0.0"
-    ],
-    extras_require={
-    "cu121": [
-        "torch==2.3.1+cu121",
-        "torchvision==0.18.1+cu121",
-        "torchaudio==2.3.1+cu121",
-    ],
     entry_points={
         "console_scripts": [
             "start_llm_server = llm_server.api.server:main",
