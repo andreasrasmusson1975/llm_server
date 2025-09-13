c97ee1bf72ff87f98a68fbc657464f76b4e5674f
andreasrasmusson1975 <38373758+andreasrasmusson1975@users.noreply.github.com>
2025-09-13 18:07:50 +0200

    Refactor install_requires and add extras_require

--- setup.py
+++ setup.py
@@ -7,17 +7,22 @@ setup(
     author="Andreas Rasmusson",
     packages=find_packages(),
     install_requires=[
-    "fastapi>=0.111.0",
-    "uvicorn>=0.30.0",
-    "torch>=2.3.0",
-    "transformers>=4.56.0",
-    "azure-identity>=1.15.0",
-    "azure-keyvault-secrets>=4.8.0",
-    "pyyaml>=6.0",
-    "bitsandbytes>=0.43.2",
-    "accelerate>=1.0.0"
-]
-,
+        "fastapi>=0.111.0",
+        "uvicorn>=0.30.0",
+        "torch>=2.3.0",
+        "transformers>=4.56.0",
+        "azure-identity>=1.15.0",
+        "azure-keyvault-secrets>=4.8.0",
+        "pyyaml>=6.0",
+        "bitsandbytes>=0.43.2",
+        "accelerate>=1.0.0"
+    ],
+    extras_require={
+    "cu121": [
+        "torch==2.3.1+cu121",
+        "torchvision==0.18.1+cu121",
+        "torchaudio==2.3.1+cu121",
+    ],
     entry_points={
         "console_scripts": [
             "start_llm_server = llm_server.api.server:main",
